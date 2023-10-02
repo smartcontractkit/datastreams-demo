c18ab61c5aae9a71a0f95ab25bd62c24d76fe8a5
Daniel Gruesso <daniel.gruesso@gmail.com>
2023-10-02 15:22:12 +0200

    Update description

--- src/config/site.tsx
+++ src/config/site.tsx
@@ -1,6 +1,6 @@
 export const siteConfig = {
-  name: "Chainlink LLDF Demo",
-  description: "Chainlink Low Latency Data Feed Demo",
+  name: "Chainlink Data Streams Demo dApp",
+  description: "Demo dApp running Chainlink Data Streams in a fullstack environment.",
   links: {
     github: "https://github.com/smartcontractkit/low-latency-demo",
     twitter: "https://twitter.com/chainlink",
