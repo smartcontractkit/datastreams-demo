0366bec6ca51d4499c9d1eb156e6163fa2c6d36d
Milen Radkov <milenradkov@me.com>
2023-10-30 23:07:26 +0200

    feat(scripts): correct up-to-date proxy address

--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -3,11 +3,12 @@ import { ethers, upgrades } from "hardhat";
 async function main() {
     const [signer] = await ethers.getSigners();
     const router = "0xab7664500b19a7a2362Ab26081e6DfB971B6F1B0";
-    const verifier = "0xea9B98Be000FBEA7f6e88D08ebe70EbaAD10224c";
+    const verifier = '0xcB1241Fdf26501fA7A2d47d841dcF72C3CAa9dCe';
     const linkToken = "0xe39Ab88f8A4777030A534146A9Ca3B52bd5D43A3";
 
     const feedsId = [
-        "0x00023496426b520583ae20a66d80484e0fc18544866a5b0bfee15ec771963274",
+        '0x00029584363bcf642315133c335b3646513c20f049602fc7d933be0d3f6360d3',
+        '0x0002c407f448ffe50a15fd5f1ffe4791830c5f8fa39cd971a3d6ae337aef51a0',
     ];
 
     const Consumer = await ethers.getContractFactory("DataFeedsConsumer");
